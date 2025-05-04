95bd86df4153debb810ab6ced13461d7031def07
Jim Remsik <jim@beflagrant.com>
2025-05-04 16:01:13 -0500

    Cleanup; Typo

--- db/migrate/20250504205741_create_products.rb
+++ db/migrate/20250504205741_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[8.0]
   def change
     create_table :products do |t|
-      t.string : title
+      t.string :title
       t.text :description
       t.decimal :price, precision: 8, scale: 2
 
