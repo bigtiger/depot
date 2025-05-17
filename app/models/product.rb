bb1cb5825dc19cd4e16457309e0aae7395cb3c66
Jim Remsik <jim@beflagrant.com>
2025-05-17 15:44:06 -0500

    Iterations D1 & D2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   has_one_attached :image
   after_commit -> { broadcast_refresh_later_to "products" }
   validates :title, :description, :image, presence: true
@@ -13,4 +15,12 @@ class Product < ApplicationRecord
     end
   end
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line Items present")
+      throw :abort
+    end
+  end
 end
